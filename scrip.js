07e01b912ea8cd51a94dfdda361482d279ac6e7c
alexisntr33 <alecruxz83@gmail.com>
2025-11-14 03:38:36 -0600

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,7 @@
+
+function error(){
+alert("Prueba a:\n\n •Desactivar el modo avión\n•Se Perdío la conexión entre tú y ella\n •Activar los datos móviles o la conexión Wi-Fi\n • Comprobar la señal en tu zona\n\n\n\ERR_INTERNET_DISCONNECTED");
+
+  
+
+}
